08b6139a4fce73a24d2e8f998be2530ab39c7dd1
JB-Gra <timishayd@gmail.com>
2023-05-02 21:44:40 -0600

    Modificación de index.html y  estilos añadidos. El archivo index.js en proceso.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+// Aquí empieza mi código
+
+const cifrarBtn = document.getElementById('cifrar');
+const descifrarBtn = document.getElementById('descifrar');
+const key = document.getElementById('key');
+
+// Key
+
+// Código para cifrar
+
+// Código para descifrar
